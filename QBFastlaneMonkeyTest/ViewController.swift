0d6617d5dc00e451a21f3368a306c9a184e527eb
Jarvis <jarvis@entertech.cn>
2016-11-02 16:35:02 +0800

    Initial Commit

---
+++ QBFastlaneMonkeyTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QBFastlaneMonkeyTest
+//
+//  Created by Jarvis on 2016/11/2.
+//  Copyright © 2016年 Hangzhou Enter Electronic Technology Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
